cd9f6985991cd8442e60334b731c23dc477828fb
Ahmet Dönmez <ahmttdonmez@gmail.com>
2020-10-15 22:50:42 +0300

    Updates

---
+++ precache-manifest.3276a234a06f115b7dbc4ca552029589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df518ac99194a5ea191c7a8d8d825bd1",
+    "url": "/temp/index.html"
+  },
+  {
+    "revision": "8bb69efa07298b78ac10",
+    "url": "/temp/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1def542df3adcc3e1d37",
+    "url": "/temp/static/js/2.ef9b312c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/temp/static/js/2.ef9b312c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bb69efa07298b78ac10",
+    "url": "/temp/static/js/main.cfbe6b87.chunk.js"
+  },
+  {
+    "revision": "b919546872414e0a9bfb",
+    "url": "/temp/static/js/runtime-main.e00e5723.js"
+  }
+]);
